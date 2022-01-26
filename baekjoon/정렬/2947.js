d466e9cc076380019b98fcd81b8f8c6d67442695
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-01-27 02:55:37 +0900

    220127 02:55

--- baekjoon/정렬/2947.js
+++ baekjoon/정렬/2947.js
@@ -1,2 +1,31 @@
 let fs = require('fs')
-let input = fs.readFileSync('예제.txt').toString().split('\n')
+let input = fs.readFileSync('dev/stdin').toString().split('\n')
+
+const arr = input[0].split(' ').map(item => Number(item))
+let temp
+let count = 0
+let ans_str = ''
+
+while (true) {
+    if (count >= arr.length-1) break;
+
+    count++
+
+    for (let i = 0; i < arr.length -1; i++) {
+        if(arr[i] > arr[i+1]) {
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+    
+            // 교체했다면 할때마다 출력
+            for (let j = 0; j < arr.length; j++) {
+                if (j===arr.length -1) {
+                    ans_str += arr[j];
+                }
+                else ans_str += arr[j] + ' '
+            }
+            console.log(ans_str)
+            ans_str = ''
+        }
+    }
+}
