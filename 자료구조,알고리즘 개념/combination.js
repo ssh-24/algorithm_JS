fc53ff1c34973395201a89aeeb1c12688dcc6fad
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-04-28 23:17:19 +0900

    220428 23:17

--- 자료구조,알고리즘 개념/combination.js
+++ 자료구조,알고리즘 개념/combination.js
@@ -1,7 +1,7 @@
 // 조합
 let input = [1,2,3,4]
 let output = []
-let count = 0
+let count = 0;
 
 // 반복문
 const combination1 = (arr) => {
@@ -14,5 +14,23 @@ const combination1 = (arr) => {
 }
 combination1(input);
 console.log(count);
+console.log("========================");
 
+
+count = 0;
 // 재귀
+const combination2 = (arr, data, s, idx, r) => {
+    if (s === r) {
+        count++;
+        console.log(data);
+        return;
+    }
+
+    for (let i = idx; arr.length - i >= r - s; i++) {
+        data[s] = arr[i];
+        combination2(arr,data,s+1,i+1,r);
+    }
+}
+
+combination2(input,output,0,0,2);
+console.log(count);
